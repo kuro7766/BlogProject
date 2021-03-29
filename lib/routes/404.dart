cca79e5bfbcb1237878088ba9a230561efa94a2c
kuro <7445841+kuro7766@user.noreply.gitee.com>
2021-03-29 20:54:21 +0800

    modify tip

--- lib/routes/404.dart
+++ lib/routes/404.dart
@@ -4,7 +4,7 @@ class Route404 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(body: Center(
-      child: Text('你要找的资源不存在'),
+      child: Text('你要找的资源不存在\n请检查链接是否正确'),
     ),);
   }
 }
