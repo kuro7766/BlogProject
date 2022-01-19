57106bf2d0e9b20aec4e98fe4ee72362632520cf
kuro <7445841+kuro7766@user.noreply.gitee.com>
2022-01-19 19:59:54 +0800

    pc compat

--- lib/config.dart
+++ lib/config.dart
@@ -1,7 +1,11 @@
 class Cfg{
-  static  bool get isMobile=>true;
+  static var __isMobile=false;
+  static  bool get isMobile=>__isMobile;
   // static  bool get isMobile=>false;
-  static set isMobile(bool value){}
+  // static  bool get isMobile=>true;
+  static set isMobile(bool value){
+    __isMobile=value;
+  }
 
 //  mobile home padding
   static const mobileHomeIconPadding=30.0;
