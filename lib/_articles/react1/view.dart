1253f5792620e6484bfb95609fdbe4d82264c955
kuro <7445841+kuro7766@user.noreply.gitee.com>
2022-02-12 22:01:15 +0800

    up

--- lib/_articles/react1/view.dart
+++ lib/_articles/react1/view.dart
@@ -1,21 +1,29 @@
 import 'package:blog_project/routes/article/unified_impl.dart';
 import 'package:blog_project/util/annotation/widget_article_meta_data.dart';
+import 'package:blog_project/util/getx_debug_tool.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 import 'logic.dart';
-
-@ArticleMetaData({"title": "B", "tag": "", "date": "2022-02-12 19:15:42"})
-class React1Component extends StatelessWidget with UnifiedWritingImpl {
-  final logic = Get.put(React1Logic());
-  final state = Get.find<React1Logic>().state;
+@ArticleMetaData({
+  "title": "Test",
+  "tag":"",
+  "date": "2022-02-12 21:20:54"
+})
+class React1Component extends StatefulWidget with UnifiedWritingImpl{
+  @override
+  _React1ComponentState createState() => _React1ComponentState();
 
   @override
-  Widget get descriptionWidgetContent => Column(
-        children: [
-          md('计数器测试'),
-        ],
-      );
+  // TODO: implement descriptionWidgetContent
+  Widget get descriptionWidgetContent => Column(children: [
+    md('计数器测试')
+  ],);
+}
+
+class _React1ComponentState extends State<React1Component> with UnifiedWritingImpl{
+  final logic = Get.put(React1Logic());
+  final state = Get.find<React1Logic>().state;
 
   @override
   Widget build(BuildContext context) {
@@ -34,5 +42,12 @@ class React1Component extends StatelessWidget with UnifiedWritingImpl {
         );
       }),
     );
+  }
+
+  @override
+  void dispose() {
+    Dbg.log('delete');
+    Get.delete<React1Logic>();
+    super.dispose();
   }
 }
